2a7bc55c4a22cad9b13b3bc2681ad3e9d903aa9d
Ben_Christie <ben.christie11@gmail.com>
2022-12-06 19:37:14 +0000

    data being pulled from elo rankiung website

--- scraper/elo-ranking-scraper.py
+++ scraper/elo-ranking-scraper.py
@@ -1,2 +1,8 @@
-# download beautiful soup, a python library that is used for web scraping using pip install beautifulsoup4
-# download requests as well using pip install requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get('https://www.eloratings.net/')
+
+teamData = driver.find_elements(By.CLASS_NAME, 'ui-widget-content')
